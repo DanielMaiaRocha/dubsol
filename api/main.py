b4b05b026663a5a4c22b9ca8e1e38cc9d5b66dba
gabrielbitencourtt <bitencourtpaula.gabriel@gmail.com>
2025-04-14 20:28:22 -0300

    ajuste api send_mail

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import smtplib
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import db_interaction as dbi
@@ -14,63 +15,38 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-def receber_emails():
+def enviar_email(remetente, destinatario, assunto, corpo):
     try:
-        servidor = imaplib.IMAP4_SSL("imap.gmail.com")
-        servidor.login(os.getenv('SMTP_USER'), os.getenv('SMTP_PASSWORD'))
-        servidor.select("inbox")
-        destinatario = os.getenv('SMTP_USER')
-        
-        # Buscar e-mails não lidos
-        status, mensagens = servidor.search(None, 'UNSEEN')
-        mensagens = mensagens[0].split()
+        msg = EmailMessage()
+        msg['From'] = remetente
+        msg['To'] = destinatario
+        msg['Subject'] = assunto
+        msg.set_content(corpo)
 
-        emails = []
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(os.getenv('SMTP_USER'), os.getenv('SMTP_PASSWORD'))
+            smtp.send_message(msg)
 
-        for num in mensagens:
-            status, msg_data = servidor.fetch(num, "(RFC822)")
-
-            for response_part in msg_data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_bytes(response_part[1])
-                    remetente = msg.get("From")
-                    assunto, encoding = decode_header(msg.get("Subject"))[0]
-                    if isinstance(assunto, bytes):
-                        assunto = assunto.decode(encoding if encoding else "utf-8")
-
-                    corpo = ""
-                    if msg.is_multipart():
-                        for part in msg.walk():
-                            if part.get_content_type() == "text/plain":
-                                corpo = part.get_payload(decode=True).decode()
-                                break
-                    else:
-                        corpo = msg.get_payload(decode=True).decode()
-                    
-                    dbi.message_log(remetente, corpo, assunto, destinatario)  # Insere o novo paciente no banco de dados
-
-
-                    emails.append({
-                        "remetente": remetente,
-                        "assunto": assunto,
-                        "corpo": corpo
-                    })
-
-        servidor.close()
-        servidor.logout()
-
-        return emails
+        return {"status": "E-mail enviado com sucesso"}
 
     except Exception as e:
-        return [{"erro": str(e)}]
+        return {"erro": str(e)}
 
 @app.route('/api/emails', methods=['GET'])
 def listar_emails():
-    emails = receber_emails()
-    return jsonify(emails)
+    return jsonify()
 
 @app.route('/api/send-email', methods=['POST'])
 def send_email():
+    data = request.get_json()
+    remetente = os.getenv('SMTP_USER')  # ou data['remetente'] se vier do front
+    destinatario = data['destinatario']
+    assunto = data['assunto']
+    corpo = data['corpo']
+
+    enviar_email(remetente, destinatario, assunto, corpo)
+
+    return jsonify('Email enviado com sucesso!')
     
 
 if __name__ == '__main__':
